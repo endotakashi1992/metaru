9b34b7374cb9ffa985e871b25a403b21099279ea
Your Name <you@example.com>
2015-12-30 01:41:34 +0900

    cors

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-console.log("hello")
 var MetaInspector = require('node-metainspector');
-var client = new MetaInspector("http://www.lifehacker.jp/", { timeout: 5000 });
 
-client.on("fetch", function(){
-    console.log("Description: " + client.description);
 
-});
+var express = require('express')
+var app = express()
 
-client.on("error", function(err){
-    console.log(error);
-});
 
-client.fetch();
+
+
+
+function simpleStringify (object){
+    var simpleObject = {};
+    for (var prop in object ){
+        if (!object.hasOwnProperty(prop)){
+            continue;
+        }
+        if (typeof(object[prop]) == 'object'){
+            continue;
+        }
+        if (typeof(object[prop]) == 'function'){
+            continue;
+        }
+        simpleObject[prop] = object[prop];
+    }
+    return simpleObject // returns cleaned up JSON
+    // return JSON.stringify(simpleObject); // returns cleaned up JSON
+};
+
+app.get('/', function (req, res) {
+  result = {}
+    var client = new MetaInspector(req.query.url, { timeout: 5000 });
+
+    client.on("fetch", function(){
+      Object.keys(client).map((key)=>{
+        if( typeof(client[key]) != 'function' ){
+          result[key] = client[key]
+        }
+      })
+      console.log(simpleStringify(result))
+      result = simpleStringify(result)
+      res.send(result)
+
+    });
+
+    client.on("error", function(err){
+        console.log(err);
+    });
+
+    client.fetch();
+        // res.send("result")
+
+})
+
+app.listen(3000)
